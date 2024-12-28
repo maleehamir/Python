12ad89b9b008d5551b9befe976acf2e9a2857cc8
maleehamir <maleehamir009@gmil.com>
2024-12-28 19:16:07 +0500

    hellllllllo commit

---
+++ Data types in python/Typecasting.py
@@ -0,0 +1,16 @@
+name = "Maleeha"
+age = 13
+weight = 45.4
+print("Name:", name)
+print("Age:" , age)
+print("weight:" , weight)
+print("type of name:", type(name))
+print("type of age:", type(age))
+print("type of weight:", type(weight))
+print("after typecasting")
+age = str(age)
+weight = int(weight)
+print("Age:" , age)
+print("weight:" , weight)
+print("type of age:", type(age))
+print("type of weight:", type(weight))

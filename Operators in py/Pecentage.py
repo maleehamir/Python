116cacca7df5584f252fb27790a88dfca9247950
maleehamir <maleehamir009@gmil.com>
2024-12-29 11:54:22 +0500

    helllllllllo commit

---
+++ Operators in py/Pecentage.py
@@ -0,0 +1,7 @@
+s=int(input("Enter the marks in Science:"))
+m=int(input("Enter the marks in Maths:"))
+u=int(input("Enter the marks in Urdu:"))
+e=int(input("Enter the marks in English:"))
+sum=s+m+u+e
+percentage=(sum/400)*100
+print("The percentage is:", percentage)
